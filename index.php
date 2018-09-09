f360c76cc0be622d62371664a2da3a475348cf0f
Garred Buehl Murphy <gmurphy@csumb.edu>
2018-09-09 02:21:43 +0000

    nearly done with lab 1

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <html><body>
+    
+    <head>
+            <title>Garred Murphy's CST 336 website</title>
+    </head>
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
+
 
 echo '<h1> Garred Murphy</h1>';
 
 ?>
+
+
+<a href="Labs/Lab1/index.html">Lab 1</a>
+
+
 </body>
 </html>
