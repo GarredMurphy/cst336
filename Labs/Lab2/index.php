cb4f24ce4d6dcaa945c296f9222a297b677f8cc8
Garred Buehl Murphy <gmurphy@csumb.edu>
2018-09-12 20:04:30 +0000

    assignment 1, not validated yet

---
+++ Labs/Lab2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+        <?php
+            $random = rand(0,2);
+            
+            
+            if (random == 0)
+            {
+                $symbol = "lemon";
+            } else if (random == 1)
+            {
+                $symbol = "cherry";
+            } else if (random == 2)
+            {
+                $symbol = "grapes";
+            }
+            
+        ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -12,6 +12,8 @@ echo '<h1> Garred Murphy</h1>';
 
 
 <a href="Labs/Lab1/index.html">Lab 1</a>
+<br />
+<a href="Assignments/Assignment1/index.html">Assignemnt 1</a>
 
 
 </body>
