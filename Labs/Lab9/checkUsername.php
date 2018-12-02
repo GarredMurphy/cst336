62596754b8c85dfd7bae21784fecd2bb041b6ed3
Garred Buehl Murphy <gmurphy@csumb.edu>
2018-12-02 08:15:57 +0000

    lab 9 final clean up and boostrap fix

--- Labs/Lab9/checkUsername.php
+++ Labs/Lab9/checkUsername.php
@@ -2,17 +2,11 @@
 header('Access-Control-Allow-Origin: *');
 include 'dbConnection.php';
 $dbConn = getDatabaseConnection("heroku_178dfb6517b4b02");
-
 $sql = "SELECT * FROM om_admin WHERE username =:username ";
-
 $parameters = array();
 $parameters[":username"]=$_GET["username"];
-
 $stmt = $dbConn->prepare($sql);
 $stmt->execute($parameters);
 $record = $stmt->fetch(PDO::FETCH_ASSOC);
-
-// print_r($record);
 echo json_encode($record);
-
 ?>
