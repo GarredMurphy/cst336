aa8a4cc9483b3829947c46d24b431ace6ae34c83
Garred Buehl Murphy <gmurphy@csumb.edu>
2018-12-08 04:27:44 +0000

    lab 10

--- Labs/Lab10/index.php
+++ Labs/Lab10/index.php
@@ -4,57 +4,78 @@
         <title> CSUMB: Pet Shelter </title>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	    
 	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>   
+        <!--
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        -->
+        
+        
         <style>
             body {
                 text-align: center;
+            }
+            #carousel-example-generic {
+                background-color: lightgrey;
             }
         </style>
    
     </head>
     <body>
         
-	<!--Add main menu here -->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">CSUMB</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="adoptions.php">Adoptions</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+<?php
+include 'inc/header.php';
+?>
+        <!-- Display Carousel here -->
+        
+        <?php
+        $images = ["bear", "lion", "otter", "tiger"];
+        ?>
+        
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <?php
+                    for ($i=0; $i <count($images); $i++){
+                        echo "<li data-target='#carousel-example-generic' data-slide-to = $i'";
+                        echo ($i == 0)?" class = 'active'": "";
+                        echo "></li>";
+                    }
+                    ?>
+                </ol>
+                
+                <div class ="carousel-inner" role="listbox">
+                <?php
+                    for ($i = 0; $i < count($images); $i++) {
+                        echo '<div class=" carousel-item ';
+                        echo ($i == 0)?"active": "";
+                        echo '">';
+                        echo '<img src="img/' . $images[$i] . '.jpg" >';
+                        echo "</div>";
+                    }
+                ?>
+                </div>
+
+                <a class="carousel-control-prev" href="#carousel-example-generic" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#carousel-example-generic" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </a>
+            </div>
         
         
-        <div class="jumbotron">
-          <h1> CSUMB Animal Shelter</h1>
-          <h2> The "official" animal adoption website of CSUMB </h2>
-        </div>
-        
-        <!-- Display Carousel here  -->
         
         
-        <a class="btn btn-outline-success" href="adoptions.php" role="button">Adopt Now</a>
+        <a class="btn btn-outline-success" href="pets.php" role="button">Adopt Now</a>
         <br><br><br>
-        <hr>
-        <footer>
-            
-            Disclaimer: The information on this site is fake.<br>
-            &copy; Lara 2018
-            
-        </footer>
+<?php
+include 'inc/footer.php';
+?>
         
         </body>
 
